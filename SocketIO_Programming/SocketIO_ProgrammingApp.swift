e6d79b807673b9d23cdc15c960ed1896a44bd03e
Peter Wi <peter.h.wi96@gmail.com>
2022-03-28 16:41:43 -0400

    initial commit

---
+++ SocketIO_Programming/SocketIO_ProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketIO_ProgrammingApp.swift
+//  SocketIO_Programming
+//
+//  Created by peter wi on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SocketIO_ProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
